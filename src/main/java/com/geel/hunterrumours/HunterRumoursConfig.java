15b4c86056194ed26b86454f4685b52521fdaf3c
Joshua Coffey <josh@trinsic.id>
2024-04-04 09:29:11 -0400

    Move config entries around

--- src/main/java/com/geel/hunterrumours/HunterRumoursConfig.java
+++ src/main/java/com/geel/hunterrumours/HunterRumoursConfig.java
@@ -14,9 +14,15 @@ public interface HunterRumoursConfig extends Config {
     @ConfigSection(
             name = "Hunter Tiers",
             description = "The tiers of hunters that are enabled",
-            position = 1
+            position = 3
     )
     String tiersSection = "tiersSection";
+    @ConfigSection(
+            name = "Highlights",
+            description = "Highlights for Hunters and Hunter Targets",
+            position = 4
+    )
+    String highlightSection = "highlightSection";
 
     @ConfigItem(
             position = 0,
@@ -25,6 +31,26 @@ public interface HunterRumoursConfig extends Config {
             description = "Show an overlay that lists the current hunter guild rumours when in the burrows"
     )
     default boolean showOverlay() {
+        return true;
+    }
+
+    @ConfigItem(
+            position = 1,
+            keyName = "showRumourInfoBox",
+            name = "Show Rumour Infobox",
+            description = "Whether an infobox containing your current rumour target should be displayed"
+    )
+    default boolean showInfoBox() {
+        return true;
+    }
+
+    @ConfigItem(
+            position = 2,
+            keyName = "showWorldMapLocations",
+            name = "Show World Map Locations",
+            description = "Whether the locations of your current rumour should show up on your world map"
+    )
+    default boolean showWorldMapLocations() {
         return true;
     }
 
@@ -73,103 +99,91 @@ public interface HunterRumoursConfig extends Config {
     }
 
     @ConfigItem(
-            position = 1,
+            position = 0,
             keyName = "highlightCurrentHunter",
             name = "Highlight current hunter",
-            description = "Whether the hunter that assigned your current task should be highlighted"
+            description = "Whether the hunter that assigned your current task should be highlighted",
+            section = highlightSection
     )
     default boolean highlightCurrentHunter() {
         return true;
     }
 
     @ConfigItem(
-            position = 2,
+            position = 1,
             keyName = "highlightKnownHunters",
             name = "Highlight known hunters",
-            description = "Whether hunters whose rumour is known should be highlighted"
+            description = "Whether hunters whose rumour is known should be highlighted",
+            section = highlightSection
     )
     default boolean highlightKnownHunters() {
         return true;
     }
 
     @ConfigItem(
-            position = 3,
+            position = 2,
             keyName = "highlightUnknownHunters",
             name = "Highlight unknown hunters",
-            description = "Whether hunters whose rumour is known should be highlighted"
+            description = "Whether hunters whose rumour is known should be highlighted",
+            section = highlightSection
     )
     default boolean highlightUnknownHunters() {
         return true;
     }
 
     @ConfigItem(
-            position = 4,
+            position = 3,
             keyName = "highlightHunterNPCs",
             name = "Highlight Hunter NPCs",
-            description = "Whether your current rumour target should be highlighted"
+            description = "Whether your current rumour target should be highlighted",
+            section = highlightSection
     )
     default boolean highlightHunterNPCs() {
         return true;
     }
 
     @ConfigItem(
-            position = 5,
-            keyName = "showRumourInfoBox",
-            name = "Show Rumour Infobox",
-            description = "Whether an infobox containing your current rumour target should be displayed"
-    )
-    default boolean showInfoBox() {
-        return true;
-    }
-
-	@ConfigItem(
-		position = 6,
-		keyName = "showWorldMapLocations",
-		name = "Show World Map Locations",
-		description = "Whether the locations of your current rumour should show up on your world map"
-	)
-	default boolean showWorldMapLocations() {
-		return true;
-	}
-
-    @ConfigItem(
-            position = 7,
-            keyName = "hunterNPCHighlightColor",
-            name = "Hunter NPC Highlight Color",
-            description = "Highlight color for Hunter targets"
-    )
-    default Color hunterNPCHighlightColor() {
-        return Color.GREEN;
-    }
-
-    @ConfigItem(
-            position = 8,
+            position = 4,
             keyName = "currentHunterHighlightColor",
             name = "Current hunter highlight color",
-            description = "Highlight color for hunter who assigned your current rumour"
+            description = "Highlight color for hunter who assigned your current rumour",
+            section = highlightSection
     )
     default Color currentHunterHighlightColor() {
         return Color.GREEN;
     }
 
     @ConfigItem(
-            position = 9,
+            position = 5,
             keyName = "knownHunterHighlightColor",
             name = "Known hunter highlight color",
-            description = "Highlight color for hunters whose rumours are known"
+            description = "Highlight color for hunters whose rumours are known",
+            section = highlightSection
     )
     default Color knownHunterHighlightColor() {
         return Color.YELLOW;
     }
 
     @ConfigItem(
-            position = 10,
+            position = 6,
             keyName = "unknownHunterHighlightColor",
             name = "Unknown hunter highlight color",
-            description = "Highlight color for hunters whose rumours are not known"
+            description = "Highlight color for hunters whose rumours are not known",
+            section = highlightSection
     )
     default Color unknownHunterHighlightColor() {
         return Color.RED;
+    }
+
+    @ConfigItem(
+            position = 7,
+            keyName = "hunterNPCHighlightColor",
+            name = "Hunter NPC Highlight Color",
+            description = "Highlight color for Hunter targets",
+            section = highlightSection
+    )
+    default Color hunterNPCHighlightColor() {
+        return Color.GREEN;
     }
 
 
